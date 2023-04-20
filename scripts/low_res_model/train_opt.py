f24b850750b47a31c66800da4c0175054c7c482b
Dialpuri <dialpuri.jordan@gmail.com>
2023-04-20 12:30:07 +0100

    Optimised training and model files

--- scripts/low_res_model/train_opt.py
+++ scripts/low_res_model/train_opt.py
@@ -3,7 +3,7 @@ import numpy as np
 import pandas as pd
 import gemmi
 from dataclasses import dataclass
-
+from typing import List
 import tensorflow as tf
 import tensorflow_addons as tfa
 from tqdm.keras import TqdmCallback
@@ -30,21 +30,25 @@ def sample_generator(dataset: str = "train"):
     df: pd.DataFrame = df.astype({'X': 'int', 'Y': 'int', 'Z': 'int'})
     df_np: np.ndarray = df.to_numpy()
 
-    def get_density(path: str):
+    def get_density(path: str, translation: List[int, int, int]) -> np.ndarray:
+
+        assert len(translation) == 3
+
         map: gemmi.FloatGrid = gemmi.read_ccp4_map(path).grid
         array: np.ndarray = np.array(
             map.get_subarray(
-                start=[X, Y, Z], shape=[param.shape, param.shape, param.shape]
+                start=translation, shape=[param.shape, param.shape, param.shape]
             )
         ) 
         array = array.reshape(param.shape, param.shape, param.shape, 1)
         return array
 
-    def get_atom_density(path: str): 
+    def get_atom_density(path: str, translation: List[int, int, int]) -> np.ndarray: 
+    
         map: gemmi.FloatGrid = gemmi.read_ccp4_map(path).grid
         array: np.ndarray = np.array(
             map.get_subarray(
-                start=[X, Y, Z], shape=[param.shape, param.shape, param.shape]
+                start=translation, shape=[param.shape, param.shape, param.shape]
             )
         ) 
         hot_array = tf.one_hot(array, depth=2)
@@ -55,36 +59,32 @@ def sample_generator(dataset: str = "train"):
             assert len(candidate) == 4
 
             pdb_code: str = candidate[0]
-            X: int = candidate[1]
-            Y: int = candidate[2]
-            Z: int = candidate[3]
+            translation: str = candidate[1:-1]
             
             density_path: str = os.path.join(
                 param.dataset_base_dir, pdb_code, f"{pdb_code}{names.density_file}.map"
             )
+            raw_density = get_density(density_path, translation)
 
             if atom_type == Types.base:
                 base_path: str = os.path.join(
                     param.dataset_base_dir, pdb_code, f"{pdb_code}{names.base_file}.map"
                 )
-                base_array = get_atom_density(base_path)
-                raw_density = get_density(density_path)
+                base_array = get_atom_density(base_path, translation)
                 yield raw_density, base_array
 
             if atom_type == Types.sugar:
                 sugar_path: str = os.path.join(
                     param.dataset_base_dir, pdb_code, f"{pdb_code}{names.sugar_file}.map"
                 )
-                sugar_array = get_atom_density(sugar_path)
-                raw_density = get_density(density_path)
+                sugar_array = get_atom_density(sugar_path, translation)
                 yield raw_density, sugar_array
 
             if atom_type == Types.phosphate:
                 phosphate_path: str = os.path.join(
                     param.dataset_base_dir, pdb_code, f"{pdb_code}{names.phosphate_file}.map"
                 )
-                phosphate_array = get_atom_density(phosphate_path)
-                raw_density = get_density(density_path)
+                phosphate_array = get_atom_density(phosphate_path, translation)
                 yield raw_density, phosphate_array
             
             
